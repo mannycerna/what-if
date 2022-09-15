7dc076a0b35a8c9a3e90aa95f60fbcce7df9649f
mannycerna <manuel.cerna@codeimmersives.com>
2022-09-15 11:18:54 -0500

    Modify space-boxing.js to include part of solution from class: 1. validation 2.console.log aksing user for planet entry

--- space-boxing.js
+++ space-boxing.js
@@ -2,42 +2,56 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let earthWeight = Number(prompt("Enter your weight on earth: "));
 
-let number = Number(prompt("Enter a number for the planet you are fighting in: "));
+//added validtion code from class 
+let validWeight = true;
 
-let planetFight = ["venus", "mars", "jupiter", "saturn", "uranus", "neptune"];
+if (isNaN (earthWeight) === true) {
+    console.log("Invalid weight, please enter a valid number.");
+    validWeight = false;
+}
 
-let choice = planetFight.at(number - 1);
+if (validWeight === true) {
+console.log("Please enter the number of the planet you will be traveling to using the following system. 1-Venus, 2-Mars, 3-Jupiter, 4-Saturn, 5-Uranus, 6-Neptune: ");
+
+let planetNum = Number(prompt("Enter planet > "));
+
+// let number = Number(prompt("Enter a number for the planet you are fighting in: "));
+
+let planetFight = ["Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+
+let choice = planetFight.at(planetNum - 1);
 
 let planets = {
-     "venus"    : 0.78,
-     "mars"     : 0.39,
-     "jupiter"  : 2.65, 
-     "saturn"   : 1.17,
-     "uranus"   : 1.05,
-     "neptune"  : 1.23
+     "Venus"    : 0.78,
+     "Mars"     : 0.39,
+     "Jupiter"  : 2.65, 
+     "Saturn"   : 1.17,
+     "Uranus"   : 1.05,
+     "Neptune"  : 1.23
 };
 
 let relGravity = Object.keys(planets).map(function(i) {
 return planets[i]*earthWeight;})
 
 
-if (number === 1) {
-    console.log(`You entered ${earthWeight} and fighting in planet ${choice}. Your weight in ${choice}    is:, ${relGravity[0]} lbs.`);
-} else if  (number === 2) {
-    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice}  is:, ${relGravity[1]} lbs.`);
+if (planetNum === 1) {
+    console.log(`You entered ${earthWeight} and fighting in planet ${choice}. Your weight in ${choice}    is: ${relGravity[0]} lbs.`);
+} else if  (planetNum === 2) {
+    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice}  is: ${relGravity[1]} lbs.`);
 
-} else if  (number === 3) {
-    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is:, ${relGravity[2]} lbs.`);
+} else if  (planetNum === 3) {
+    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is: ${relGravity[2]} lbs.`);
 
-} else if  (number === 4) {
-    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is:, ${relGravity[3]} lbs.`);
+} else if  (planetNum === 4) {
+    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is: ${relGravity[3]} lbs.`);
 
-} else if  (number === 5) {
-    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is:, ${relGravity[4]} lbs.`);
+} else if  (planetNum === 5) {
+    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is: ${relGravity[4]} lbs.`);
 
-} else if  (number === 6) {
-    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is:, ${relGravity[5]} lbs.`);
+} else if  (planetNum === 6) {
+    console.log(`You entered ${earthWeight} and fighting in planet ${choice}.  Your weight in ${choice} is: ${relGravity[5]} lbs.`);
 
+}
 }
 
 //Test code below:
@@ -56,3 +70,47 @@ if (number === 1) {
 // if (number === 1) {
 //     console.log(`You entered ${earthWeight} and destination planet ${key}`);
 // }
+
+/*  Solution from class on 8.15.2022
+const prompt = require('prompt-sync') ({sigint: true});
+
+let weight = Number(prompt("Enter your Earth weight: "));
+
+let validWeight = true;
+
+if (isNaN (weight) === true) {
+    console.log("Invalid weight");
+    validWeight = false;
+}
+
+if (validWeight === true) {
+console.log("Please enter the number of the planet you will be traveling to using the following system. 1-Venus, 2-Mars, 3-Jupiter, 4-Saturn, 5-Uranus, 6-Neptune: ");
+
+let planetNum = Number(prompt("> "));
+
+console.log(weight);
+console.log(planetNum);
+
+if (planetNum === 1) {
+    console.log("You picked Venus.")
+    console.log(`Your weight is: ${weight * 0.78}`)
+} else if (planetNum === 2) {
+    console.log("You picked Mars.")
+    console.log(`Your weight is: ${weight * 0.39}`)
+} else if (planetNum === 3) {
+    console.log("You picked Jupiter.")
+    console.log(`Your weight is: ${weight * 2.65}`)
+} else if (planetNum === 4) {
+    console.log("You picked Saturn.")
+    console.log(`Your weight is: ${weight * 1.17}`)
+} else if (planetNum === 5) {
+    console.log("You picked Uranus.")
+    console.log(`Your weight is: ${weight * 1.05}`)
+} else if (planetNum === 6) {
+    console.log("You picked Neptune.")
+    console.log(`Your weight is: ${weight * 1.23}`)
+} else {
+    console.log("Invalid entry!")
+}
+}
+*/
