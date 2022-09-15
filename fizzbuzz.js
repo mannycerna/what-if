83bb5e464c12b6f749f6e29f291251fe58f3b24b
mannycerna <manuel.cerna@codeimmersives.com>
2022-09-15 11:39:08 -0500

    Update fizzbuzz.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,11 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+/*
+divisible by 3 -> write fizz
+divisible by 5 -> write buzz
+divisible by both -> write fizzbuzz -->This evaluation needs to be entered before the others or it will never reach the final condition.  The first condition will be satisifed and exit the rest of the if statements.
+*/
+
 let number = Number(prompt("Enter a number: "));
 
 if (number % 3 === 0 && number % 5 == 0) {
@@ -11,3 +17,4 @@ if (number % 3 === 0 && number % 5 == 0) {
 } else {
     console.log(number);
 }
+
